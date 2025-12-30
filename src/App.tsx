5c448db98eda7402a211c0a174d376c2f16193e8
Сидоров Владимир Станиславович <vsidorov@nevatom.ru>
2025-12-30 15:53:34 +0700

    adds admin pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { Route, Routes } from 'react-router';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import routes from './utils/routes';
-import ProtectedRoute from './components/ProtectedRoute';
-import RegisterPage from './pages/RegisterPage';
-import ProfilePage from './pages/ProfilePage';
-import NavBar from './components/NavBar';
-import { Container } from '@mui/material';
-import AdminPage from './pages/AdminPage';
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import routes from "./utils/routes";
+import ProtectedRoute from "./components/ProtectedRoute";
+import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
+import NavBar from "./components/NavBar";
+import { Container } from "@mui/material";
+import AdminPage from "./pages/AdminPage";
+import UsersPage from "./pages/admin/UsersPage";
 
 function App() {
   return (
@@ -16,18 +17,23 @@ function App() {
       <Container
         maxWidth="lg"
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          minHeight: '80vh',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "80vh",
         }}
       >
         <Routes>
           <Route element={<ProtectedRoute />}>
             <Route path={routes.home()} element={<HomePage />}></Route>
             <Route path={routes.profile()} element={<ProfilePage />}></Route>
-            <Route path={routes.admin.root()} element={<AdminPage />}></Route>
+            <Route path={routes.admin.root()} element={<AdminPage />}>
+              <Route
+                path={routes.admin.users()}
+                element={<UsersPage />}
+              ></Route>
+            </Route>
           </Route>
           <Route path={routes.login()} element={<LoginPage />}></Route>
           <Route path={routes.register()} element={<RegisterPage />}></Route>
