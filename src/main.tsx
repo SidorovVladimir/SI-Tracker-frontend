e74d70ae4214e3d83c6f0a237e6f45ce71039972
Vladimir Sidorov <v.sidorov29091988@gmail.com>
2026-01-03 22:26:32 +0700

    feat: add notistack and secure user update mutation
    - Added notistack (^3.0.2) for snackbar notifications in React app
    - Added UpdateUserMutation to allow updating only firstName and lastName

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { HttpLink } from "@apollo/client";
-import { ApolloProvider } from "@apollo/client/react";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { HttpLink } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+import { SnackbarProvider } from 'notistack';
 
 const getClient = () => {
   return new ApolloClient({
     link: new HttpLink({
-      uri: "http://localhost:4000/graphql",
-      credentials: "include",
+      uri: 'http://localhost:4000/graphql',
+      credentials: 'include',
     }),
     cache: new InMemoryCache(),
   });
@@ -18,14 +19,23 @@ const getClient = () => {
 
 const bootstrap = () => {
   const client = getClient();
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <ApolloProvider client={client}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ApolloProvider>
-    </StrictMode>
+  createRoot(document.getElementById('root')!).render(
+    <SnackbarProvider
+      maxSnack={1}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      autoHideDuration={2000}
+    >
+      <StrictMode>
+        <ApolloProvider client={client}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ApolloProvider>
+      </StrictMode>
+    </SnackbarProvider>
   );
 };
 
--- src/pages/admin/EditUserPage.tsx
+++ src/pages/admin/EditUserPage.tsx
@@ -11,8 +11,18 @@ import {
 } from '@mui/material';
 import { useState } from 'react';
 import { useParams } from 'react-router';
-import { GetUserDocument } from '../../graphql/types/__generated__/graphql';
-import { useQuery } from '@apollo/client/react';
+import {
+  GetUserDocument,
+  GetUserQuery,
+  UpdateUserDocument,
+} from '../../graphql/types/__generated__/graphql';
+import { useMutation, useQuery } from '@apollo/client/react';
+import { enqueueSnackbar } from 'notistack';
+
+type FieldErrors = {
+  firstName?: string;
+  lastName?: string;
+};
 
 export default function EditUserPage() {
   const { userId } = useParams<{ userId: string }>();
@@ -21,28 +31,6 @@ export default function EditUserPage() {
     variables: { id: userId! },
     skip: !userId,
   });
-
-  const user = data?.user;
-
-  const [form, setForm] = useState({
-    firstName: user?.firstName || '',
-    lastName: user?.lastName || '',
-  });
-
-  if (user && (!form.firstName || !form.lastName)) {
-    setForm({
-      firstName: user.firstName || '',
-      lastName: user.lastName || '',
-    });
-  }
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
 
   if (loading)
     return (
@@ -57,6 +45,62 @@ export default function EditUserPage() {
         Ошибка загрузки пользователя: {error.message}
       </Alert>
     );
+  if (!data?.user) return <Alert>Пользователь не найден</Alert>;
+
+  return <UserForm key={userId} user={data.user} />;
+}
+
+function UserForm({ user }: { user: NonNullable<GetUserQuery['user']> }) {
+  const [form, setForm] = useState({
+    firstName: user.firstName || '',
+    lastName: user.lastName || '',
+  });
+  const [fieldErrors, setFieldErrors] = useState<FieldErrors>({});
+
+  const [updateUser, { loading: updating }] = useMutation(UpdateUserDocument, {
+    onCompleted: () => {
+      enqueueSnackbar('Пользователь успешно обновлен', {
+        variant: 'success',
+      });
+    },
+    onError: (error) => {
+      try {
+        const parsed = JSON.parse(error.message);
+        if (Array.isArray(parsed)) {
+          const errors: FieldErrors = {};
+          parsed.forEach((err) => {
+            if (err.path.includes('firstName')) {
+              errors.firstName = err.message;
+            }
+            if (err.path.includes('lastName')) {
+              errors.lastName = err.message;
+            }
+          });
+          setFieldErrors(errors);
+        }
+      } catch {
+        enqueueSnackbar(`Ошибка обновления: ${error.message}`, {
+          variant: 'error',
+        });
+      }
+    },
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+    if (fieldErrors[name as keyof FieldErrors]) {
+      setFieldErrors((prev) => ({ ...prev, [name]: undefined }));
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setFieldErrors({});
+    await updateUser({
+      variables: { id: user.id, input: form },
+    });
+  };
 
   return (
     <Box sx={{ maxWidth: 600, mx: 'auto', mt: 3, mb: 6 }}>
@@ -88,6 +132,9 @@ export default function EditUserPage() {
               fullWidth
               variant="outlined"
               size="small"
+              required
+              error={!!fieldErrors.firstName}
+              helperText={fieldErrors.firstName}
             />
 
             <TextField
@@ -98,13 +145,22 @@ export default function EditUserPage() {
               fullWidth
               variant="outlined"
               size="small"
+              required
+              error={!!fieldErrors.lastName}
+              helperText={fieldErrors.lastName}
             />
 
             <Divider sx={{ my: 2 }} />
 
             <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-              <Button type="submit" variant="contained" size="large">
-                Сохранить изменения
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+                disabled={updating}
+                startIcon={updating && <CircularProgress size={16} />}
+              >
+                {updating ? 'Сохранение...' : 'Сохранить изменения'}
               </Button>
             </Box>
           </Stack>
